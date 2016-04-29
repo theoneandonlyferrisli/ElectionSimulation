fe5ed18056fd92e036745225e1ffbba7ee39d9c0
Yuefeng Li <radek@uw.edu>
2016-04-29 16:15:55 -0700

    Added skeleton

--- StateObject.py
+++ StateObject.py
@@ -5,19 +5,22 @@ class State:
         # constants
         self.name = name
         self.elector_count = elector_count
+
+        # Randomly generated from -15 to 15,
+        # which will later be added to steve_support
         self.gdp = random.randrange(-15, 16)
+
+        # Ideology 0 = liberal and 1 = conservative
         self.idealogy = random.randrange(0, 2)
-        self.has_incident = random.randrange(0, 5)
-        self.incident_type = random.randrange(-5, 5)
         
         # variables
         self.open = True
         self.steve_support = 0
         self.opponent_support = 0
         if self.gdp > 0:
-          steve_support = self.gdp
+            self.steve_support = self.gdp
         else:
-          opponent_support = self.gdp * -1
+            self.opponent_support = self.gdp * -1
         self.multiplier = 1
         self.groud_operation_index = 0 # max is 5
         self.community_outreach_event = 0 # max is 2
@@ -25,6 +28,7 @@ class State:
 
 
     def update_support(self):
+        'A function invoked after a move is made.'
         has_incident = random.randrange(0, 5)
         if has_incident == 0:
             self.steve_support += random.randrange(-5, 6)
--- Test.py
+++ Test.py
@@ -5,5 +5,6 @@ def test():
     state1 = State("Washington", 1)
     print(state1.name)
     print(state1.steve_support)
+    print(state1.opponent_support)
     state1.update_support()
 test()
